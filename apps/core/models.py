1aa737a911a3697e557971a63ec728824835c235
Albino Ndjonale <albinondjonale1@gmail.com>
2024-10-08 16:53:11 +0100

    Create the Client Model

--- apps/core/models.py
+++ apps/core/models.py
@@ -15,3 +15,24 @@ class Service(models.Model):
     class Meta:
         verbose_name        = 'Service'
         verbose_name_plural = 'Services'
+
+class Client(models.Model):
+    '''
+      Modelo Cliente, não sera feito directamente
+    o registro de clientes mas durante o agendamento
+    o cliente devera fornecer seus dados
+    '''
+    id          = models.UUIDField(unique = True, primary_key = True)
+    name        = models.CharField(max_length = 50)
+    email       = models.EmailField(max_length = 254)
+    number      = models.CharField(max_length = 11)
+    addess      = models.CharField(max_length = 100)
+    cep         = models.CharField(max_length = 11)
+    complemento = models.CharField(max_length = 20)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name        = 'Client'
+        verbose_name_plural = 'Clients'
