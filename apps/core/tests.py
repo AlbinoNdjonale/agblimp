41eeaba363f976bbeb309c082eea564cda83a349
Albino Ndjonale <albinondjonale1@gmail.com>
2024-10-15 16:00:04 +0100

    create tests for all models and endpoint scheduling

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,3 +1,111 @@
 from django.test import TestCase
 
+from . import models
+
 # Create your tests here.
+
+class ServiceModelTest(TestCase):
+    def test_create_service(self):
+        service = models.Service(
+            name = 'Service Test',
+            price = 150,
+            duration = 1
+        )
+
+        service.save()
+
+        self.assertEqual(service.name, 'Service Test')
+        self.assertEqual(service.price, 150)
+        self.assertEqual(service.duration, 1)
+        self.assertEqual(service.description, None)
+        self.assertIn('id', service.__dict__.keys())
+
+class ClientModelTest(TestCase):
+    def test_create_client(self):
+        client = models.Client(
+            name    = 'client name',
+            number  = '555555555',
+            email   = 'example@gmail.com',
+            address = 'endereço',
+            cep     = '63.254-788'
+        )
+
+        client.save()
+
+        self.assertEqual(client.name, 'client name')
+        self.assertEqual(client.number, '555555555')
+        self.assertEqual(client.email, 'example@gmail.com')
+        self.assertEqual(client.address, 'endereço')
+        self.assertEqual(client.cep, '63.254-788')
+        self.assertIn('id', client.__dict__.keys())
+
+class SchedulingModelTest(TestCase):
+    def test_create_scheduling(self):
+        service = models.Service(
+            name = 'Service Test',
+            price = 150,
+            duration = 1
+        )
+        service.save()
+
+        client = models.Client(
+            name    = 'client name',
+            number  = '555555555',
+            email   = 'example@gmail.com',
+            address = 'endereço',
+            cep     = '63.254-788'
+        )
+        client.save()
+
+        scheduling = models.Scheduling(
+            service     = service,
+            client      = client,
+            date        = '2024-10-09 13:00',
+            way_payment = 'a vista'
+        )
+
+        scheduling.save()
+
+        self.assertEqual(scheduling.service, service)
+        self.assertEqual(scheduling.client, client)
+        self.assertEqual(scheduling.date, '2024-10-09 13:00')
+        self.assertEqual(scheduling.way_payment, 'a vista')
+        self.assertEqual(scheduling.status, 'pendente')
+        self.assertIn('id', scheduling.__dict__.keys())
+
+class TestimonyModelTest(TestCase):
+    def test_create_testimony(self):        
+        testimony = models.Testimony(
+            name_client = 'nome do cliente',
+            mesage      = 'uma mensagem'
+        )
+
+        testimony.save()
+
+        self.assertEqual(testimony.name_client, 'nome do cliente')
+        self.assertEqual(testimony.mesage, 'uma mensagem')
+        self.assertEqual(testimony.status, 'pendente')
+
+class FaqModelTest(TestCase):
+    def test_create_faq(self):        
+        faq = models.Faq(
+            question = 'como',
+            response = 'assim'
+        )
+
+        faq.save()
+
+        self.assertEqual(faq.question, 'como')
+        self.assertEqual(faq.response, 'assim')
+
+class QuestionModelTest(TestCase):
+    def test_create_question(self):        
+        question = models.Question(
+            name_client = 'nome do cliente',
+            question = 'como'
+        )
+
+        question.save()
+
+        self.assertEqual(question.name_client, 'nome do cliente')
+        self.assertEqual(question.question, 'como')
--- apps/web/tests.py
+++ apps/web/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
+from apps.core import models
 
 # Create your tests here.
+
+class SchedulingIntegrationTest(TestCase):
+    @classmethod
+    def setUpData(cls):
+
+        # cria um serviço para poder ser usado nos testes
+        models.Service(
+            name        = 'nome do serviço',
+            price       = 150,
+            duration    = 1,
+            description = 'descrição do serviço'
+        ).save()
+
+    def test_create_sheduling(self):
+        response = self.client.post(reverse('web:scheduling'), {
+            'name'       : 'Nome do cliente',
+            'email'      : 'example@gmail.com',
+            'address'    : 'endereço do cliente',
+            'number'     : '555555555',
+            'cep'        : '63.254-788',
+            # este endpoint cria um cliente na
+            # base de dados a partir dos dados do formulario de agendamento
+            'way_payment': 'a vista',
+            'service'    : '1',
+            'date'       : '2024-11-17 12:00'
+        })
+
+        self.assertEqual(response.status_code, 302)
+        # verificar se o endpoint esta retornando uma resposta de redirecionamento
+        
+        self.assertTrue(models.Client.objects.filter(name = 'Nome do cliente').exists)
+        # verifica se o cliente foi criado e salvo na base de dados
