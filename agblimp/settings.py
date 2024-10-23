b4120c57997787392bbca521e684e8757af52c62
Albino Ndjonale <albinondjonale1@gmail.com>
2024-10-23 16:10:00 +0100

    feacture/settings debug dynamic

--- agblimp/settings.py
+++ agblimp/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG') == 'true'
 
 ALLOWED_HOSTS = []
 
